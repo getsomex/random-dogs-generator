d517e67e806e4f3a42a55bd5056f3513cc3021e1
getsomex <deathster786@gmail.com>
2019-11-21 16:33:44 +0900

    init commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const API = 'https://dog.ceo/api/breeds/image/random/3';
+const column = document.querySelector('.column');
+const button = document.querySelector('.button');
+const clear = document.querySelector('#clear')
+function markup(el){
+  const markup =   `
+  <div class="card">
+        <div class="card-image">
+            <figure class="image-dog">
+                <img src="${el}">
+            </figure>            
+        </div>
+    </div>`
+    return markup;
+};
+const dogs = async()=>{
+    const response = await fetch(API);
+    const json = await response.json()
+    json.message.forEach(dog=>{
+        column.insertAdjacentHTML('beforeend',markup(dog))
+    })
+
+}
+
+button.addEventListener('click',()=>{
+    dogs();
+});
+
+clear.addEventListener('click',()=>{
+  column.innerHTML = '';
+})
